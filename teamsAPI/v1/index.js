91518198865c6e26d0a8bc1938d5a923e8b9efe8
Baltasar <balramrom@alumn.us.es>
2019-12-07 10:56:40 +0000

    Added support for methods GET and POST  /cotacts

--- teamsAPI/v1/index.js
+++ teamsAPI/v1/index.js
@@ -8,9 +8,15 @@ teamsAPI.register = function(app, teamDB){
 // GET - /teams 
 app.get(BASE_API_PATH + "/teams", (req,res)=>{
     console.log(Date()+" - GET /teams");
-    res.statusCode = 200;
-    res.send([]);
-    
+    teamDB.find({},(err,teams)=>{
+        if(err){
+            console.log(Date()+" - "+err);
+            res.sendStatus(500);
+        }else{
+            res.statusCode=200;
+            res.send(teams);
+        }
+    });    
 });
 
 //POST - /teams
@@ -28,16 +34,19 @@ app.post(BASE_API_PATH+"/teams", (req,res)=>{
     });
 });
 
-/*
+
 //DELETE - /teams
 app.delete(BASE_API_PATH+"/teams", (req,res)=>{
     console.log(Date()+" DELETE /teams");
-    teams = [];
-    res.statusCode=204;
-    res.send(teams);
+    teamDB.remove({}, { multi: true }, function (err, numRemoved) {
+        if(err){
+            console.log(Date()+" - "+err);
+            res.sendStatus(500);
+        }else{
+            res.sendStatus(204);
+        }
+    });
 });
-*/
 
-//PUT - /teams
 
 }
