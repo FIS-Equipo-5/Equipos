255189a51ba47a2c290608e2acc27f5a9511905d
Baltasar <balramrom@alumn.us.es>
2019-12-13 20:04:13 +0000

    Added jest jest-extended and supertest as packages of the project for testing and jest.config.js as an example of a simple test

---
+++ tests/server.test.js
@@ -0,0 +1,11 @@
+
+describe("Hello world tests", ()=>{
+
+    it("Should do a stupid test", ()=>{
+        const a = 5;
+        const b = 3;
+        const sum = a + b;
+
+        expect(sum).toBe(10);
+    });
+});
