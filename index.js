53a759ad02d8fac14a4f243ebb40b7ec4238354b
balramrom <balramrom@alumn.us.es>
2019-12-23 15:05:23 +0000

    Changes to connect the app with mongodb

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
 const app = require('./server.js');
+const dbConnect = require('./db.js');
 
 var port = (process.env.PORT || 3000);
 
-console.log("Starting API server...");
-/*
-var teams = [
+console.log("Starting API server at " + port);
 
-    {
-        "team_id": 1,
-        "name": "Real Madrid",
-        "code": null,
-        "logo": "https://media.api-football.com/teams/541.png",
-        "country": "Spain",
-        "founded": 1902,
-        "venue_name": "Estadio Santiago Bernabéu",
-        "venue_surface": "grass",
-        "venue_address": "Avenida de Concha Espina 1, Chamartín",
-        "venue_city": "Madrid",
-        "venue_capacity": 85454
+dbConnect().then(
+    () => {
+        app.listen(port);
+        console.log("Server ready!");
     },
-    {
-        "team_id": 2,
-        "name": "Real Betis",
-        "code": null,
-        "logo": "https://media.api-football.com/teams/543.png",
-        "country": "Spain",
-        "founded": 1902,
-        "venue_name": "Benito Villamarín",
-        "venue_surface": "grass",
-        "venue_address": "Avenida Reina Mercedes",
-        "venue_city": "Sevilla",
-        "venue_capacity": 63000
+    err => {
+        console.log("Connection error: " + err);
     }
-
-];
-*/
-
-app.listen(port);
-
-console.log("Server ready!");
+);
