5d8c578496140e11d051f5dfa371424605f501e8
balramrom <balramrom@alumn.us.es>
2019-12-23 17:57:48 +0000

    fixed some import errors

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var bodyParser = require('body-parser');
 
 const Player = require('./playersAPI/model/players');
 /*Creamos variables para la ejecución de la API teams*/
-var teamsAPI = require('./teamsAPI/v1/teams');
+var teamsAPI = require('./teamsAPI/v1/index');
+/*Creamos variables para la ejecución de la API teams*/
+var playersAPI = require('./playersAPI/v1/index');
 
 var app = express();
 app.use(bodyParser.json());
