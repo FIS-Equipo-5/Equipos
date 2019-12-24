7431b6f356aa8bebddd7e97d11a3547cade4c758
balramrom <balramrom@alumn.us.es>
2019-12-24 18:21:18 +0000

    Added tests for each method of the API with mongodb

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,6 @@
 const app = require('../server.js');
-const db = require('../db.js');
+const db = require('../db.js'); /////////// NONO BORRA ESTA LÍNEA CUANDO ADAPTES LOS TEST A MONGO
+const Team = require('../teamsAPI/module/teams');
 const request = require('supertest');
 
 describe("Hello world tests", ()=>{
@@ -16,44 +17,43 @@ describe("Hello world tests", ()=>{
 describe("Teams API", ()=>{
 
     describe("GET /teams", ()=>{
-        beforeAll(()=>{
+       
+        it("Sould return all the teams that are stored in our database", () =>{
             const teams = [
 
-                {
-                    "team_id": 1,
-                    "name": "Real Madrid",
-                    "code": null,
-                    "logo": "https://media.api-football.com/teams/541.png",
-                    "country": "Spain",
-                    "founded": 1902,
-                    "venue_name": "Estadio Santiago Bernabéu",
-                    "venue_surface": "grass",
-                    "venue_address": "Avenida de Concha Espina 1, Chamartín",
-                    "venue_city": "Madrid",
-                    "venue_capacity": 85454
-                },
-                {
-                    "team_id": 2,
-                    "name": "Real Betis",
-                    "code": null,
-                    "logo": "https://media.api-football.com/teams/543.png",
-                    "country": "Spain",
-                    "founded": 1902,
-                    "venue_name": "Benito Villamarín",
-                    "venue_surface": "grass",
-                    "venue_address": "Avenida Reina Mercedes",
-                    "venue_city": "Sevilla",
-                    "venue_capacity": 63000
-                }
-            ];
-
-            dbFind = jest.spyOn(db.teamDB,"find");
+                new Team(
+                    {
+                     "team_id": 1,
+                     "name": "Real Madrid",
+                     "code": null,
+                     "logo": "https://media.api-football.com/teams/541.png",
+                     "country": "Spain",
+                     "founded": 1902,
+                     "venue_name": "Estadio Santiago Bernabéu",
+                     "venue_surface": "grass",
+                     "venue_address": "Avenida de Concha Espina 1, Chamartín",
+                     "venue_city": "Madrid",
+                     "venue_capacity": 85454
+                     }) ,
+                 new Team(
+                     {
+                     "team_id": 2,
+                     "name": "Real Betis",
+                     "code": null,
+                     "logo": "https://media.api-football.com/teams/543.png",
+                     "country": "Spain",
+                     "founded": 1902,
+                     "venue_name": "Benito Villamarín",
+                     "venue_surface": "grass",
+                     "venue_address": "Avenida Reina Mercedes",
+                     "venue_city": "Sevilla",
+                     "venue_capacity": 63000
+                 })
+             ];
+            dbFind = jest.spyOn(Team,"find");
             dbFind.mockImplementation((query, callback)=>{
                 callback(null, teams);
             });
-        });
-
-        it("Sould return all the teams that are stored in our database", () =>{
             return request(app).get("/api/v1/teams").then((response)=>{
                 expect(response.statusCode).toBe(200);
                 expect(response.body).toBeArrayOfSize(2);
@@ -61,56 +61,305 @@ describe("Teams API", ()=>{
             });
         });
     });
-
+     
 
     describe("POST /teams", () =>{
 
         const team = {
-            "team_id": 1,
-            "name": "Inserted team",
-            "code": null,
+            "team_id": 1313,
+            "name": "prueba",
+            "code": "777",
             "logo": "https://media.api-football.com/teams/541.png",
             "country": "Spain",
-            "founded": 1902,
-            "venue_name": "Estadio Santiago Bernabéu",
-            "venue_surface": "grass",
+            "founded": 1909,
+            "venue_name": "Estadio Benito Villamarín",
+            "venue_surface": "polvoraaa",
             "venue_address": "Avenida de Concha Espina 1, Chamartín",
-            "venue_city": "Madrid",
-            "venue_capacity": 85454
+            "venue_city": "Sevilla",
+            "venue_capacity": 85454,
+            "budget": 14423432,
+            "value": 10000000000000000000
         };
-        let dbInsert;
+        let dbCreate;
+        let dbFind;
 
         beforeEach(() =>{
-            dbInsert = jest.spyOn(db.teamDB,"insert");
+            dbCreate = jest.spyOn(Team,"create");
+            dbFind = jest.spyOn(Team, "find");
         });
 
-        it("Should add a new team is everything is fine", ()=>{
-
-            dbInsert.mockImplementation((t, callback)=>{
-                callback(false,t);
+        it("Should add a new team if everything it's ok", ()=>{
+            dbFind.mockImplementation((query, callback)=>{
+                emptyArray = new Array();
+                callback(null,emptyArray);
+            });
+            dbCreate.mockImplementation((t, callback)=>{
+                callback(false,new Team(t));
             });
             return request(app).post("/api/v1/teams").send(team).then((response)=>{
+                expect(dbFind).toBeCalledWith({ $or: [ {team_id: team.team_id }, { name: team.name }, {code: team.code} ] }, expect.any(Function));
+                expect(dbCreate).toBeCalledWith(team, expect.any(Function));
                 expect(response.statusCode).toBe(201);
-                expect(dbInsert).toBeCalledWith(team, expect.any(Function));
             });
         });
 
-        it("Shoud return 500 if there is some problem with the DB", ()=>{
+        it("Should return an error if the team already exist (same code, name or id)", ()=>{
+            dbFind.mockImplementation((query, callback)=>{
+                array = [new Team (team)];
+                callback(false,array);
+            });
+            return request(app).post("/api/v1/teams").send(team).then((response)=>{
+                expect(dbFind).toBeCalledWith({ $or: [ {team_id: team.team_id }, { name: team.name }, {code: team.code} ] }, expect.any(Function));
+                expect(response.statusCode).toBe(400);
+            });
+        });
 
-            dbInsert.mockImplementation((t, callback)=>{
+        it("Shoud return 500 if there is some problem with the DB - 1", ()=>{
+            dbFind.mockImplementation((query, callback)=>{
                 callback(true,null);
             });
             return request(app).post("/api/v1/teams").send(team).then((response)=>{
                 expect(response.statusCode).toBe(500);
-                expect(dbInsert).toBeCalledWith(team, expect.any(Function));
+                expect(dbFind).toBeCalledWith({ $or: [ {team_id: team.team_id }, { name: team.name }, {code: team.code} ] }, expect.any(Function));
             });
+        });
 
+        it("Shoud return 500 if there is some problem with the DB - 2", ()=>{
+            dbFind.mockImplementation((query, callback)=>{
+                array = new Array();
+                callback(false,array);
+            });
+            dbCreate.mockImplementation((t, callback)=>{
+                callback(true,t);
+            });
+            return request(app).post("/api/v1/teams").send(team).then((response)=>{
+                expect(dbFind).toBeCalledWith({ $or: [ {team_id: team.team_id }, { name: team.name }, {code: team.code} ] }, expect.any(Function));
+                expect(dbCreate).toBeCalledWith(team, expect.any(Function));
+                expect(response.statusCode).toBe(500);
+            });
+        });
+
+        it("Shoud return 400 if there is some missing attribute or exceed the number of attributes", ()=>{
+            const badTeam = {
+                "team_id": 1313,
+                "name": "prueba",
+                "code": "777",
+                "logo": "https://media.api-football.com/teams/541.png",
+                "country": "Spain",
+                "founded": 1909,
+                "venue_name": "Estadio Benito Villamarín",
+                "venue_surface": "polvoraaa",
+                "venue_address": "Avenida de Concha Espina 1, Chamartín",
+                "venue_city": "Sevilla",
+                "venue_capacity": 85454,
+                "budget": 14423432
+                }
+            return request(app).post("/api/v1/teams").send(badTeam).then((response)=>{
+                expect(response.type).toInclude('json');
+                expect(response.statusCode).toBe(400);
+                expect(response.body.err).toBe('Atribute value required');
+
+            });
         });
 
     });
 
-});
+    describe("DELETE /teams", () =>{
 
+        let dbRemove;
+
+        beforeEach(() =>{
+            dbRemove = jest.spyOn(Team,"remove");
+        });
+
+        it("Should delete all the teams", ()=>{
+            dbRemove.mockImplementation((query, callback)=>{
+                callback(false,new Object());
+            });
+            return request(app).delete("/api/v1/teams").then((response)=>{
+                expect(dbRemove).toBeCalledWith({}, expect.any(Function));
+                expect(response.statusCode).toBe(204);
+            });
+        });
+
+        it("Shoud return 500 if there is some problem with the DB", ()=>{
+            dbRemove.mockImplementation((query, callback)=>{
+                callback(true,null);
+            });
+            return request(app).delete("/api/v1/teams").then((response)=>{
+                expect(dbRemove).toBeCalledWith({}, expect.any(Function));
+                expect(response.statusCode).toBe(500);
+            });
+        });
+
+    });
+
+    describe("GET /teams/{team_name}", ()=>{
+        let dbFindOne;
+        const team = {
+            "team_id": 1313,
+            "name": "prueba",
+            "code": "777",
+            "logo": "https://media.api-football.com/teams/541.png",
+            "country": "Spain",
+            "founded": 1909,
+            "venue_name": "Estadio Benito Villamarín",
+            "venue_surface": "polvoraaa",
+            "venue_address": "Avenida de Concha Espina 1, Chamartín",
+            "venue_city": "Sevilla",
+            "venue_capacity": 85454,
+            "budget": 14423432,
+            "value": 10000000000000000000
+        };
+        beforeEach(() =>{
+            dbFindOne = jest.spyOn(Team,"findOne");
+        });
+        it("Should return the searched team", ()=>{
+            dbFindOne.mockImplementation((query, callback)=>{
+                callback(false,new Team(team));
+            });
+            return request(app).get("/api/v1/teams/prueba").then((response)=>{
+                expect(response.statusCode).toBe(200);
+                expect(dbFindOne).toBeCalledWith({name: team.name}, expect.any(Function));
+            });
+        });
+
+        it("Should return 404 if the team does not exist", ()=>{
+            dbFindOne.mockImplementation((query, callback)=>{
+                callback(false,null);
+            });
+            return request(app).get("/api/v1/teams/prueba").then((response)=>{
+                expect(response.statusCode).toBe(404);
+                expect(dbFindOne).toBeCalledWith({name: team.name}, expect.any(Function));
+            });
+        });
+
+        it("Shoud return 500 if there is some problem with the DB", ()=>{
+            dbFindOne.mockImplementation((query, callback)=>{
+                callback(true,null);
+            });
+            return request(app).get("/api/v1/teams/prueba").then((response)=>{
+                expect(response.statusCode).toBe(500);
+                expect(dbFindOne).toBeCalledWith({name: team.name}, expect.any(Function));
+            });
+        });
+    });
+
+    describe("PUT /teams/{team_name}", ()=>{
+
+        const team = {
+            "team_id": 1313,
+            "name": "prueba",
+            "code": "777",
+            "logo": "https://media.api-football.com/teams/541.png",
+            "country": "Spain",
+            "founded": 1909,
+            "venue_name": "Estadio Benito Villamarín",
+            "venue_surface": "polvoraaa",
+            "venue_address": "Avenida de Concha Espina 1, Chamartín",
+            "venue_city": "Sevilla",
+            "venue_capacity": 85454,
+            "budget": 14423432,
+            "value": 10000000000000000000
+        };
+        const updatedTeam = {
+            "team_id": 1313,
+            "name": "prueba",
+            "code": "777",
+            "logo": "https://media.api-football.com/teams/541.png",
+            "country": "Spain",
+            "founded": 1999,
+            "venue_name": "Estadio Benito Villamarín",
+            "venue_surface": "polvoraaa",
+            "venue_address": "Avenida de Concha Espina 1, Chamartín",
+            "venue_city": "Sevilla",
+            "venue_capacity": 85454,
+            "budget": 14423432,
+            "value": 10000000000000000000
+        };
+
+        const badUpdatedTeam = {
+            "team_id": 1312,
+            "name": "prueba",
+            "code": "777",
+            "logo": "https://media.api-football.com/teams/541.png",
+            "country": "Spain",
+            "founded": 1999,
+            "venue_name": "Estadio Benito Villamarín",
+            "venue_surface": "polvoraaa",
+            "venue_address": "Avenida de Concha Espina 1, Chamartín",
+            "venue_city": "Sevilla",
+            "venue_capacity": 85454,
+            "budget": 14423432,
+            "value": 10000000000000000000
+        };
+
+        beforeEach(() =>{
+            dbFindOne = jest.spyOn(Team, "findOne");
+            dbFindOne.mockImplementation((query, callback)=>{
+                callback(false,team);
+            });
+        });
+
+        it("Should update the team if everything it's ok", ()=>{
+            
+            dbUpdateOne = jest.spyOn(Team, "updateOne");
+
+            
+            dbUpdateOne.mockImplementation((query,updtTeam,callback)=>{
+                callback(false,new Object());
+            });
+
+            return request(app).put("/api/v1/teams/prueba").send(updatedTeam).then((response)=>{
+                expect(response.statusCode).toBe(200);
+                expect(dbFindOne).toBeCalledWith({name: team.name}, expect.any(Function));
+                expect(dbUpdateOne).toBeCalledWith({name: team.name}, updatedTeam, expect.any(Function));
+            });
+        });
+
+        it("Should return 400 if the team id, code or name has been modified", ()=>{
+
+            return request(app).put("/api/v1/teams/prueba").send(badUpdatedTeam).then((response)=>{
+                expect(response.statusCode).toBe(400);
+                expect(dbFindOne).toBeCalledWith({name: team.name}, expect.any(Function));
+                expect(response.body.err).toBe("The updated team must have the same id, name and code");
+            });
+        });
+    });
+
+    describe("DELETE /teams/{team_name}", ()=>{
+
+        beforeEach(()=>{
+            dbDeleteOne = jest.spyOn(Team, "deleteOne");
+        });
+
+        it("Should delete the team if everything it's ok", ()=>{
+            dbDeleteOne.mockImplementation((query, callback)=>{
+                callback(false, new Object());
+            });
+            return request(app).delete("/api/v1/teams/prueba").then((response)=>{
+                expect(response.statusCode).toBe(204);
+                expect(dbDeleteOne).toBeCalledWith({name: "prueba"}, expect.any(Function));
+            });
+        });
+
+        it("Shoud return 404 the team does not exist", ()=>{
+            dbDeleteOne.mockImplementation((query, callback)=>{
+                var obj = new Object();
+                obj['deletedCount'] = 0;
+                callback(false, obj);
+            });
+            return request(app).delete("/api/v1/teams/prueba").then((response)=>{
+                expect(response.statusCode).toBe(404);
+                expect(dbDeleteOne).toBeCalledWith({name: "prueba"}, expect.any(Function));
+            });
+        });
+
+    });
+
+
+});
+/*
 describe("Players API", () => {
     describe("GET /players", () => {
         beforeAll(() => {
@@ -214,3 +463,4 @@ describe("Players API", () => {
         });
     });
 });
+*/
