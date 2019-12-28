e0e77382407841ea08eaefa960a33cf2fb9ce4ca
balramrom <balramrom@alumn.us.es>
2019-12-28 19:18:36 +0000

    adding port 27017 to DB_URL

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
 //Conexión a la base dee datos
-const DB_URL = (process.env.MONGO_URL || 'mongodb://mongo/test');
+const DB_URL = (process.env.MONGO_URL || 'mongodb://localhost:27017/teamsDB');
 
 //Función de conexión a la base de datos
 const dbConnect = function(){
